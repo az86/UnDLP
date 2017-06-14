25811f534f75502c99477e795758163dda5d7e03
az <m_sunqi@qq.com>
2017-06-14 15:14:13 +0800

    new feather autolauncher

---
+++ AutoLauncher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "Uedit32.exe"
+                }
+            };
+            p.Start();
+        }
+    }
+}
